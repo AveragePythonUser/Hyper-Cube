913ac0529006b61bfd06ca9cbb643e3e3e51a2a9
AveragePythonUser <109878558+AveragePythonUser@users.noreply.github.com>
2023-06-22 21:23:26 -0700

    finished pause menu
    remember to set Time.timeScale to 1 when changing scenes. if you dont stuff breaks

--- Tommy - Hyper Cube/Assets/Scripts/hud_and_pause.cs
+++ Tommy - Hyper Cube/Assets/Scripts/hud_and_pause.cs
@@ -1,22 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class hud_and_pause : MonoBehaviour
 {
 
     public GameObject HUD;
     public GameObject pause_menu;
+    public GameObject controls;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        pause_menu.SetActive(false);
+        controls.SetActive(false);
     }
 
     public void pause_button()
@@ -31,6 +27,29 @@ public class hud_and_pause : MonoBehaviour
         resume_game();
         pause_menu.SetActive(false);
         HUD.SetActive(true);
+    }
+
+    public void main_menu_button()
+    {
+        pause_menu.SetActive(false);
+        controls.SetActive(false);
+        PlayerPrefs.SetFloat("player_resets", 0);
+        // make sure to set timescale to 1 when changing scenes
+        // if you dont stuff breaks
+        resume_game();
+        SceneManager.LoadScene("main_menu");
+    }
+
+    public void controls_button()
+    {
+        pause_menu.SetActive(false);
+        controls.SetActive(true);
+    }
+
+    public void done_button()
+    {
+        controls.SetActive(false);
+        pause_menu.SetActive(true);
     }
 
     void pause_game()
